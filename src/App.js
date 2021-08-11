cebcb7e2a35831c6e904d8ad4d8c16f5077b2820
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-08-11 22:24:35 +0530

    [Pradip] ADDED:Reset Password Form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 // import logo from './logo.svg';
 // import './App.css';
+// import ForgotPassword from './Pages/ForgotPassword/ForgotPassword';
+// import Login from './Pages/Login/Login';
 import Login from './Pages/Login/Login';
-import Registration from './Pages/Regisration/Registration';
+// import Registration from './Pages/Regisration/Registration';
+// import Resetpassword from './Pages/ResetPassword/Resetpassword';
 
 function App() {
   return (
     <div>
 
-      {/* <Login></Login> */}
-      <Registration/>
+      <Login></Login>
+{/* 
+      <Login></Login> */}
+       {/* <Registration/>  */}
+       {/* <ForgotPassword></ForgotPassword>
+       <Resetpassword></Resetpassword> */}
 
     </div>
   );
---
+++ src/Pages/ResetPassword/Resetpassword.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Resetpassword.css';
+
+export class Resetpassword extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Resetpassword
