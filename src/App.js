26e51d1d066f79cb33ba83a0876f660de77a00b9
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-08-11 23:47:27 +0530

    [Pradip]ADDED:reset form in fundoo with validation n proper ui implementation

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@
 // import './App.css';
 // import ForgotPassword from './Pages/ForgotPassword/ForgotPassword';
 // import Login from './Pages/Login/Login';
-import Login from './Pages/Login/Login';
+// import Login from './Pages/Login/Login';
 // import Registration from './Pages/Regisration/Registration';
-// import Resetpassword from './Pages/ResetPassword/Resetpassword';
+
+import { Resetpassword } from "./Pages/ResetPassword/Resetpassword";
+
 
 function App() {
   return (
     <div>
-
-      <Login></Login>
+  <Resetpassword></Resetpassword>
+      {/* <Login></Login> */}
 {/* 
       <Login></Login> */}
        {/* <Registration/>  */}
--- src/Pages/ResetPassword/Resetpassword.jsx
+++ src/Pages/ResetPassword/Resetpassword.jsx
@@ -1,14 +1,114 @@
 import React, { Component } from 'react'
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+// import FormHelperText from "@material-ui/core/FormHelperText";
 import './Resetpassword.css';
 
 export class Resetpassword extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          password: "",
+          confirm: "",
+          passwordError: false,
+          confirmError: false,
+        //   showPassword: false,
+        //   showPassword1: false,
+        };
+      }
+    
+      validation = () => {
+        let isError = false;
+        const errors= this.state;
+        errors.passwordError= this.state.password === '' ? true : false;
+        errors.confirmError= this.state.confirm === '' ? true : false;
+
+        this.setState({
+            ...errors,
+        })
+        return isError = errors.confirmError || errors.passwordError
+
+    }
+
+    Reset = () => {
+        var isValidated = this.validation();
+        if(!isValidated){
+            console.log("validation successfull");
+        }
+    }
+
+    change = e => {
+        this.setState({
+          [e.target.name]: e.target.value
+        });
+      }
+   
+     
     render() {
         return (
-            <div>
-                
-            </div>
-        )
-    }
+
+            <div className="first-container">
+        <div className="resetpassword-container">
+          <div className="fundoo-sign">
+            <span className="letterF">F</span>
+            <span className="letteru">u</span>
+            <span className="lettern">n</span>
+            <span className="letterd">d</span>
+            <span className="letterO">o</span>
+            <span className="lettero">o</span>
+          </div>
+          <div class="topname">
+          <h3 className="heading-sign">Account reovery</h3>
+          <h4 className="heading-account">Recover your Fundoo Account</h4>
+          </div>
+          <div className="email1">
+          <TextField 
+                    error = {this.state.passwordError}
+                    label="Password"
+                    type="text"
+                    name="password"
+                    variant="outlined"
+                    fullWidth
+                    onChange={e => this.change(e)}
+                    helperText={this.state.passwordError ? "Enter Valid Password" : ''}/>
+            <br></br>
+            <br></br>
+          </div>
+          <div className="email1">
+           
+            <TextField 
+                    error = {this.state.confirmError}
+                    label="Confirm"
+                    type="text"
+                    name="confirm"
+                    fullWidth
+                    variant="outlined"
+                    onChange={e => this.change(e)}
+                    helperText={this.state.confirmError ? "Confirmed Password" : ''} />
+                    <div>
+          </div>
+        
+
+           <div className="Resetbtn">
+            <Button
+              variant="contained"
+              className="next"
+              color="primary"
+              onClick={this.Reset}
+            >
+              Reset
+            </Button>
+          </div> 
+        </div>
+      </div>
+      );
+  
+  </div>
+)
 }
+}
+
+ 
 
 export default Resetpassword
