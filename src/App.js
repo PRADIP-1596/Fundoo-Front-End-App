20f120b5e87383d7964bcb8556444189db876b25
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-08-21 00:45:20 +0530

    [Pradip]Addition:Changes UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
 // import ForgotPassword from './Pages/ForgotPassword/ForgotPassword';
-import Login from './Pages/Login/Login';
+import Login from './Pages/login/Login';
 // import Login from './Pages/Login/Login';
 import Registration from './Pages/Regisration/Registration';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ForgotPassword from "./Pages/ForgotPassword/ForgotPassword";
 import { Resetpassword } from "./Pages/ResetPassword/Resetpassword";
- import Dashboard from './Pages/Dashboard/DashBoard';
+//  import Dashboard from './Pages/Dashboard/DashBoard';
 import CreateNote from './Pages/KeepComponent/CreateNote/CreateNote';
 import DisplayNotes from './Pages/KeepComponent/DisplayNotes/DisplayNotes';
 import IconButton from './Pages/KeepComponent/IconButton/IconButton';
 import Notes from './Pages/KeepComponent/Notes/Notes';
+import Dashboard from './components/Dashboard/Dashboard';
+
 
 
 function App() {
@@ -25,8 +27,9 @@ function App() {
           <Route exact path="/Registration" component={Registration}></Route>
           <Route exact path="/Resetpassword" component={Resetpassword}></Route>
           <Route exact path="/ForgotPassword" component={ForgotPassword}></Route>  
-     {/* <Dashboard/>  */}
-      <Notes/>
+     <Dashboard/> 
+      {/* <Notes/> */}
+    
       {/* <Archieve/> */}
         </Switch>
       </Router>
